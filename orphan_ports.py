ef77e3bfbe4acc42bb59ed9ab71e51983da32f87
Russell Johnston <rujohns2@cisco.com>
2023-08-18 09:21:59 -0600

    error handling formating

--- orphan_ports.py
+++ orphan_ports.py
@@ -62,6 +62,81 @@ def run_commands(device, commands, mpQueue):
         mpQueue.put(result)
 
 
+def orphan_ports(switch):
+    if "AuthenticationError" or "ConnectionTimeOut" in switch.values():
+        for k, v in switch.items():
+            data = {
+                "hostname": v,
+                "mgmt-ip": k,
+                "vpc-vlan": "",
+                "orphan-port": "",
+                "lldp-neighbor": "",
+                "lldp-neighbor-mgmt-ip": "",
+            }
+            return data
+    for k, v in switch.items():
+        ip_addr = v["mgmt-ip"]
+        neighbors = v["show lldp neighbors | json"]["TABLE_nbor"]["ROW_nbor"]
+        if "Empty JSON" not in v["show vpc orphan-ports | json"]:
+            if isinstance(
+                v["show vpc orphan-ports | json"]["TABLE_orphan_ports"][
+                    "ROW_orphan_ports"
+                ],
+                list,
+            ):
+                for vlan in v["show vpc orphan-ports | json"]["TABLE_orphan_ports"][
+                    "ROW_orphan_ports"
+                ]:
+                    ports = [x.strip() for x in vlan["vpc-orphan-ports"].split(",")]
+                    for port in ports:
+                        neighbor = search_neighbors(neighbors, port)
+                        data = {
+                            "hostname": k,
+                            "mgmt-ip": ip_addr,
+                            "vpc-vlan": vlan["vpc-vlan"],
+                            "orphan-port": port,
+                        }
+                        if neighbor is None:
+                            data["lldp-neighbor"] = ""
+                            data["lldp-neighbor-mgmt-ip"] = ""
+                        else:
+                            data["lldp-neighbor"] = neighbor["chassis_id"]
+                            data["lldp-neighbor-mgmt-ip"] = neighbor["mgmt_addr"]
+                        return data
+            else:
+                ports = v["show vpc orphan-ports | json"]["TABLE_orphan_ports"][
+                    "ROW_orphan_ports"
+                ]["vpc-orphan-ports"].split(",")
+                for port in ports:
+                    neighbor = search_neighbors(neighbors, port)
+                    data = {
+                        "hostname": k,
+                        "mgmt-ip": ip_addr,
+                        "vpc-vlan": v["show vpc orphan-ports | json"][
+                            "TABLE_orphan_ports"
+                        ]["ROW_orphan_ports"]["vpc-vlan"],
+                        "orphan-port": port,
+                    }
+                    if neighbor is None:
+                        data["lldp-neighbor"] = ""
+                        data["lldp-neighbor-mgmt-ip"] = ""
+                    else:
+                        data["lldp-neighbor"] = neighbor["chassis_id"]
+                        data["lldp-neighbor-mgmt-ip"] = neighbor["mgmt_addr"]
+                    return data
+        else:
+            # Handle Switches with no Orphan Ports
+            data = {
+                "hostname": k,
+                "mgmt-ip": ip_addr,
+                "vpc-vlan": "N/A",
+                "orphan-port": "None Found",
+                "lldp-neighbor": "",
+                "lldp-neighbor-mgmt-ip": "",
+            }
+            return data
+
+
 def search_neighbors(neighbors, port):
     for neighbor in neighbors:
         if neighbor["l_port_id"] == port:
@@ -136,67 +211,71 @@ if __name__ == "__main__":
         Each dictionary will be added to the output list var for display
         in tables and csv report as determined by cli flags (-t and/or -r).
         """
-        for k, v in switch.items():
-            ip_addr = v["mgmt-ip"]
-            neighbors = v["show lldp neighbors | json"]["TABLE_nbor"]["ROW_nbor"]
-            if "Empty JSON" not in v["show vpc orphan-ports | json"]:
-                if isinstance(
-                    v["show vpc orphan-ports | json"]["TABLE_orphan_ports"][
-                        "ROW_orphan_ports"
-                    ],
-                    list,
-                ):
-                    for vlan in v["show vpc orphan-ports | json"]["TABLE_orphan_ports"][
-                        "ROW_orphan_ports"
-                    ]:
-                        ports = [x.strip() for x in vlan["vpc-orphan-ports"].split(",")]
-                        for port in ports:
-                            neighbor = search_neighbors(neighbors, port)
-                            data = {
-                                "hostname": k,
-                                "mgmt-ip": ip_addr,
-                                "vpc-vlan": vlan["vpc-vlan"],
-                                "orphan-port": port,
-                            }
-                            if neighbor is None:
-                                data["lldp-neighbor"] = ""
-                                data["lldp-neighbor-mgmt-ip"] = ""
-                            else:
-                                data["lldp-neighbor"] = neighbor["chassis_id"]
-                                data["lldp-neighbor-mgmt-ip"] = neighbor["mgmt_addr"]
-                            output.append(data)
-                else:
-                    ports = v["show vpc orphan-ports | json"]["TABLE_orphan_ports"][
-                        "ROW_orphan_ports"
-                    ]["vpc-orphan-ports"].split(",")
-                    for port in ports:
-                        neighbor = search_neighbors(neighbors, port)
-                        data = {
-                            "hostname": k,
-                            "mgmt-ip": ip_addr,
-                            "vpc-vlan": v["show vpc orphan-ports | json"][
-                                "TABLE_orphan_ports"
-                            ]["ROW_orphan_ports"]["vpc-vlan"],
-                            "orphan-port": port,
-                        }
-                        if neighbor is None:
-                            data["lldp-neighbor"] = ""
-                            data["lldp-neighbor-mgmt-ip"] = ""
-                        else:
-                            data["lldp-neighbor"] = neighbor["chassis_id"]
-                            data["lldp-neighbor-mgmt-ip"] = neighbor["mgmt_addr"]
-                        output.append(data)
-            else:
-                # Handle Switches with no Orphan Ports
-                data = {
-                    "hostname": k,
-                    "mgmt-ip": ip_addr,
-                    "vpc-vlan": "N/A",
-                    "orphan-port": "None Found",
-                    "lldp-neighbor": "",
-                    "lldp-neighbor-mgmt-ip": "",
-                }
-                output.append(data)
+        output.append(orphan_ports(switch))
+
+        # for k, v in switch.items():
+        #     if "Empty JSON" not in v["show vpc orphan-ports | json"]:
+        #         if isinstance(
+        #             v["show vpc orphan-ports | json"]["TABLE_orphan_ports"][
+        #                 "ROW_orphan_ports"
+        #             ],
+        #             list,
+        #         ):
+        #             ip_addr = v["mgmt-ip"]
+        #             neighbors = v["show lldp neighbors | json"]["TABLE_nbor"][
+        #                 "ROW_nbor"
+        #             ]
+        #             for vlan in v["show vpc orphan-ports | json"]["TABLE_orphan_ports"][
+        #                 "ROW_orphan_ports"
+        #             ]:
+        #                 ports = [x.strip() for x in vlan["vpc-orphan-ports"].split(",")]
+        #                 for port in ports:
+        #                     neighbor = search_neighbors(neighbors, port)
+        #                     data = {
+        #                         "hostname": k,
+        #                         "mgmt-ip": ip_addr,
+        #                         "vpc-vlan": vlan["vpc-vlan"],
+        #                         "orphan-port": port,
+        #                     }
+        #                     if neighbor is None:
+        #                         data["lldp-neighbor"] = ""
+        #                         data["lldp-neighbor-mgmt-ip"] = ""
+        #                     else:
+        #                         data["lldp-neighbor"] = neighbor["chassis_id"]
+        #                         data["lldp-neighbor-mgmt-ip"] = neighbor["mgmt_addr"]
+        #                     output.append(data)
+        #         else:
+        #             ports = v["show vpc orphan-ports | json"]["TABLE_orphan_ports"][
+        #                 "ROW_orphan_ports"
+        #             ]["vpc-orphan-ports"].split(",")
+        #             for port in ports:
+        #                 neighbor = search_neighbors(neighbors, port)
+        #                 data = {
+        #                     "hostname": k,
+        #                     "mgmt-ip": ip_addr,
+        #                     "vpc-vlan": v["show vpc orphan-ports | json"][
+        #                         "TABLE_orphan_ports"
+        #                     ]["ROW_orphan_ports"]["vpc-vlan"],
+        #                     "orphan-port": port,
+        #                 }
+        #                 if neighbor is None:
+        #                     data["lldp-neighbor"] = ""
+        #                     data["lldp-neighbor-mgmt-ip"] = ""
+        #                 else:
+        #                     data["lldp-neighbor"] = neighbor["chassis_id"]
+        #                     data["lldp-neighbor-mgmt-ip"] = neighbor["mgmt_addr"]
+        #                 output.append(data)
+        #     else:
+        #         # Handle Switches with no Orphan Ports
+        #         data = {
+        #             "hostname": k,
+        #             "mgmt-ip": ip_addr,
+        #             "vpc-vlan": "N/A",
+        #             "orphan-port": "None Found",
+        #             "lldp-neighbor": "",
+        #             "lldp-neighbor-mgmt-ip": "",
+        #         }
+        #         output.append(data)
 
     if args.csv_report:
         d = datetime.now()
